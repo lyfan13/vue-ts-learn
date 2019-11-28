63aba70fffc10dada6f922312eae28575d4014c3
lyfan13 <46513492+lyfan13@users.noreply.github.com>
2019-11-28 22:14:16 +0800

    update

---
+++ src/components/Dialog/Dialog.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="Dialog">
+    <div class="container">
+      <p class="content">{{content}}</p>
+      <div class="btn-wrap">
+        <div class="btn cancel" @click="handleCancel">{{cancelText}}</div>
+        <div class="btn confirm" @click="handleConfirm">{{confirmText}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Dialog',
+    props: {
+      confirmText: {
+        type: String,
+        default: '确定'
+      },
+      cancelText:{
+        type:String,
+        default:'取消'
+      },
+      content:{
+        type:String,
+        default:'确定不再考虑一下？'
+      },
+      duration:{
+        type:Number,
+        default:1000
+      }
+    },
+    methods: {
+      handleConfirm() {
+        this.$emit('on-confirm')
+      },
+      handleCancel(){
+        this.$emit('cancel')
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+#Dialog{
+  position: fixed;
+  left: 0;
+  right: 0;
+  top:0;
+  bottom: 0;
+  background: rgba($color: #000000, $alpha: .6);
+}
+.container{
+  position: absolute;
+  top:50%;
+  left:50%;
+  transform: translate3d(-50%,-50%,0);
+  background: white;
+  border-radius: 8px;
+  font-size: 15px;
+}
+.content{
+  padding: 25px 20px;
+  text-align: center;
+  border-bottom: 1px solid #ddd;
+}
+.btn-wrap{
+  display: flex;
+}
+.btn{
+  flex:1;
+  text-align: center;
+  padding: 12px 0;
+}
+.cancel{
+  color:#999;
+  border-right: 1px solid #ddd;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,25 +5,36 @@
       src="../assets/logo.png"
       @click="handleClick"
     >
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <Button text="haha" @click="handleClick"/>
+    <Button text="显示dialog" bgColor="#f84d90" @click="showDialog"/>
+    <Dialog v-if="isShowDialog" @cancel="isShowDialog = false" content="你好傻逼哈哈哈哈"/>
+    <i class="icon">&#xe8bf;</i>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import Button from '@/components/Button.vue'
+import Dialog from '@/components/Dialog/Dialog.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
-    Button
+    Button,
+    Dialog
+  },
+  data() {
+    return {
+      isShowDialog: false
+    }
   },
   methods: {
     handleClick() {
       this.$toast('别跑！明天请你吃饭',1000,()=>console.log('callback'))
+    },
+    showDialog(){
+      console.log('showDialog')
+      this.isShowDialog = true;
     }
   },
 }
